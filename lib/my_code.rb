1de02c990bba8417f953bc0fac1dc7db9e55642d
Emily Lillie <emilylillie3@gmail.com>
2019-06-22 12:18:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ def map_to_square(source_array)
   return new
 end
     
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   new = starting_point
   while i < source_array.length do
     new += source_array[i]
